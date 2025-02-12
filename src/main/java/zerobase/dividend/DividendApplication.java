f693cced7508c4f908059fa6688c77065e6740e3
조수빈 <whtnqls03@gmail.com>
2025-02-12 17:19:12 +0900

    250212 Trie를 이용한 자동완성

--- src/main/java/zerobase/dividend/DividendApplication.java
+++ src/main/java/zerobase/dividend/DividendApplication.java
@@ -8,7 +8,5 @@ public class DividendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DividendApplication.class, args);
-
     }
-
 }
--- src/main/java/zerobase/dividend/web/CompanyController.java
+++ src/main/java/zerobase/dividend/web/CompanyController.java
@@ -18,8 +18,9 @@ public class CompanyController {
     private final CompanyService companyService;
 
     @GetMapping("/autocomplete")
-    public ResponseEntity<?> autocomplete(@RequestParam String keyword) {
-        return null;
+    public ResponseEntity<?> autocomplete(@RequestParam(value = "keyword") String keyword) {
+        var result = this.companyService.autocomplete(keyword);
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping
@@ -35,7 +36,7 @@ public class CompanyController {
             throw new RuntimeException("Ticker is empty");
         }
         Company company = this.companyService.save(ticker);
-
+        this.companyService.addAutocompleteKeyword(company.getName());
         return ResponseEntity.ok(company);
     }
 
